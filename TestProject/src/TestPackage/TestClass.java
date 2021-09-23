aa49f685010dd42f5295fdafd6b6f8206faec59a
santhoshp77 <santhoshp77@LAPTOP-QMHGJ7OD>
2021-09-23 19:52:47 +0530

    test

--- TestProject/src/TestPackage/TestClass.java
+++ TestProject/src/TestPackage/TestClass.java
@@ -5,5 +5,6 @@ public class TestClass {
 	public static void main(String args[])
 	{
 		System.out.println("this is the first statement");
+		System.out.println("this is the second statement");
 	}
 }
